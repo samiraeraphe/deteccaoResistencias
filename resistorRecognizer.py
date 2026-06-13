e50acbd50a89e6dbefc281e4c4219d8a82f75aed
Maria Luiza <malualvesbarbosa25@gmail.com>
2026-06-13 16:12:39 -0300

    Segmmentação do resistor (passo 2) ok

--- resistorRecognizer.py
+++ resistorRecognizer.py
@@ -44,3 +44,65 @@ else:
     cv2.destroyAllWindows()
 
     #Passo 2: Isolar o Resistor (Segmentação)
+    ''''
+    # Não deu certo usando Canny, mas deixo o código aqui pra referência futura
+
+    gray_image = cv2.cvtColor(filtered_image, cv2.COLOR_BGR2GRAY)
+    bordes = cv2.Canny(gray_image, 50, 150)
+
+    # Encontrar os contornos
+    contours, _ = cv2.findContours(bordes, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        # Encontrar o maior contorno (assumindo que seja o resistor)
+        largest_contour = max(contours, key=cv2.contourArea)
+
+        # Obter as coordenadas do retângulo delimitador
+        x, y, largura, altura = cv2.boundingRect(largest_contour)
+
+        # Recortar a imagem para isolar o resistor
+        cropped_image = filtered_image[y:y+altura, x:x+largura]
+
+        # Mostrar a imagem recortada
+        cv2.imshow('Bordas', bordes)
+        cv2.imshow('Cropped Resistor', cropped_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print("Nenhum contorno encontrado.")
+    '''
+    # Não deu certo usando Canny
+    # Mudei para o método do repositório https://github.com/SupreethRao99/CVResist.git:
+    imagem_cinza = cv2.cvtColor(filtered_image, cv2.COLOR_BGR2GRAY)
+
+    # Usando os mesmos parâmetrosdo repositório:
+    # - 79 é o tamanho do "bloco" que ele analisa por vez (precisa ser número ímpar).
+    # - 2 é um ajuste fino (subtraído da média do bloco).
+    mascara_fundo = cv2.adaptiveThreshold(
+        imagem_cinza, 
+        255, # Se for diferente do fundo, pinta de branco (255)
+        cv2.ADAPTIVE_THRESH_MEAN_C, 
+        cv2.THRESH_BINARY, 
+        79, 
+        2
+    )
+
+    # Inverter a máscara para ficar com o fundo preto e resistor branco
+    mascara_resistor = cv2.bitwise_not(mascara_fundo)
+
+    # Preenchendo os buracos com Fechamento Morfológico
+    # Quadrado 75x75 pixels para engolir as faixas
+    kernel = np.ones((75, 75), np.uint8)
+    
+    # Operação de Fechamento (MORPH_CLOSE)
+    mascara_solida = cv2.morphologyEx(mascara_resistor, cv2.MORPH_CLOSE, kernel)
+
+    # Teste para verificar as máscaras
+    cv2.imshow("4 - Mascara Global (Com buracos)", mascara_resistor)
+    cv2.imshow("5 - Mascara Solida (Corrigida)", mascara_solida)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # Passo 3: Cores HSV
+    
